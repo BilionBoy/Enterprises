509c55b58a0c6d30b7f667e89f00775cb01c210b
Dhiogo <dhiogoandre26@gmail.com>
2025-01-28 11:04:36 -0400

    Devise: Layout controller e validações no Model

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Relacionamentos
+  belongs_to :funcao
+  has_many :users_estabelecimentos, dependent: :destroy
+  has_many :estabelecimentos, through: :users_estabelecimentos
+
+  # Validações
+  validates :nome,     presence: true
+  validates :email,    presence: true
+  validates :cpf,      presence: true, uniqueness: true, format: { with: /\A\d{11}\z/, message: "deve conter 11 dígitos numéricos" }
+  validates :telefone, presence: true
+end
