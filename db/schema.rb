3f77fe79222ee0d6d628b10ec4f4db774ae13ac1
Dhiogo <dhiogoandre26@gmail.com>
2025-01-28 10:26:23 -0400

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_28_140757) do
+ActiveRecord::Schema[7.2].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "funcoes", force: :cascade do |t|
-    t.string "nome"
-    t.string "descricao"
-    t.string "created_by"
-    t.string "updated_by"
-    t.datetime "deleted_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
