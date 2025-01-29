782c949115facb2328a38b6819d0e06cc26dcd8e
BilionBoy <dhiogoandre26@gmail.com>
2025-01-29 00:08:04 -0400

    inflection: user_estabelecimento

--- app/controllers/estabelecimentos_controller.rb
+++ app/controllers/estabelecimentos_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
+
 class EstabelecimentosController < ApplicationController
   before_action :set_estabelecimento, only: %i[show edit update destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
 
- def index
+  def index
     @q = Estabelecimento.ransack(params[:q])
     @pagy, @estabelecimentos = pagy(@q.result)
   end
@@ -20,7 +21,7 @@ class EstabelecimentosController < ApplicationController
     @estabelecimento = Estabelecimento.new(estabelecimento_params)
 
     if @estabelecimento.save
-      redirect_to estabelecimentos_path, notice: t('messages.created_successfully')
+      redirect_to estabelecimentos_path, notice: t("messages.created_successfully")
     else
       render :new, status: :unprocessable_entity
     end
@@ -28,7 +29,7 @@ class EstabelecimentosController < ApplicationController
 
   def update
     if @estabelecimento.update(estabelecimento_params)
-      redirect_to estabelecimentos_path, notice: t('messages.updated_successfully'), status: :see_other
+      redirect_to estabelecimentos_path, notice: t("messages.updated_successfully"), status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
@@ -36,25 +37,25 @@ class EstabelecimentosController < ApplicationController
 
   def destroy
     if @estabelecimento.destroy
-      redirect_to estabelecimentos_url, notice: t('messages.deleted_successfully')
+      redirect_to estabelecimentos_url, notice: t("messages.deleted_successfully")
     else
-      redirect_to estabelecimentos_url, alert: t('messages.delete_failed_due_to_dependencies')
-    end   
+      redirect_to estabelecimentos_url, alert: t("messages.delete_failed_due_to_dependencies")
+    end
   end
 
   private
 
   def set_estabelecimento
     @estabelecimento = Estabelecimento.find_by(id: params[:id])
-    return redirect_to estabelecimentos_path, alert: t('messages.not_found') unless @estabelecimento
+    redirect_to estabelecimentos_path, alert: t("messages.not_found") unless @estabelecimento
   end
 
   def estabelecimento_params
-    permitted_attributes = Estabelecimento.column_names.reject { |col| ['deleted_at', 'created_by', 'updated_by'].include?(col) }
+    permitted_attributes = Estabelecimento.column_names.reject { |col| [ "deleted_at", "created_by", "updated_by" ].include?(col) }
     params.require(:estabelecimento).permit(permitted_attributes.map(&:to_sym))
   end
 
   def handle_not_found
-    redirect_to estabelecimentos_path, alert: t('messages.not_found')
+    redirect_to estabelecimentos_path, alert: t("messages.not_found")
   end
 end
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,6 +1,8 @@
 # config/initializers/inflections.rb
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.irregular "user", "users"
   inflect.irregular "funcao", "funcoes"
   inflect.irregular "estabelecimento", "estabelecimentos"
+  inflect.irregular "user_estabelecimento", "user_estabelecimentos"
 end
